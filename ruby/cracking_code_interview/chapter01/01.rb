275208eca84ea8eff88a2e0cfece819ff5e93490
CodeConscious <hello@codeconscio.us>
2022-09-11 11:54:36 +0900

    Various minor improvements for style

--- ruby/cracking_code_interview/chapter01/01.rb
+++ ruby/cracking_code_interview/chapter01/01.rb
@@ -2,26 +2,24 @@
 
 # Displays the text and results on the terminal.
 def print_results(text)
-    print %Q[   "#{text}" -> ]
-    puts has_unique_chars(text)
+  print "   #{text} -> "
+  puts all_chars_unique?(text)
 end
 
-# Returns a bool indicating whether all string characters are unique.
-def has_unique_chars(text)
-    chars = text.split('')
-    used_chars = []
+# Returns a bool indicating whether all characters in a string are unique.
+def all_chars_unique?(text)
+  chars = text.split('')
+  used_chars = []
 
-    chars.each do |letter|
-        if used_chars.include? letter
-            return false;
-        else
-            used_chars.push(letter)
-        end
-    end
+  chars.each do |letter|
+    return false if used_chars.include?(letter)
 
-    return true;
+    used_chars.push(letter)
+  end
+
+  true
 end
 
-print_results("abcdefghijklmnopqrstuvwxyz")
-print_results("aabcdefghijklmnopqrstuvwxyz")
-print_results("abcdefghijklmnopqrstuvwxyza")
+print_results('abcdefghijklmnopqrstuvwxyz')
+print_results('aabcdefghijklmnopqrstuvwxyz')
+print_results('abcdefghijklmnopqrstuvwxyza')
